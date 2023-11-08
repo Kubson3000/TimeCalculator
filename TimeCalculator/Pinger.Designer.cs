68148a97704f8ae57ef3271af954c1e92f78cd5e
Kubson3000 <jakub.monka@royal-flowers.pl>
2023-11-08 14:46:52 +0100

    auto ping

---
+++ TimeCalculator/Pinger.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TimeCalculator
+{
+    partial class Pinger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            ping_button = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 154);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // ping_button
+            // 
+            ping_button.Location = new Point(12, 172);
+            ping_button.Name = "ping_button";
+            ping_button.Size = new Size(215, 23);
+            ping_button.TabIndex = 1;
+            ping_button.Text = "Ping";
+            ping_button.UseVisualStyleBackColor = true;
+            ping_button.Click += ping_button_Click;
+            // 
+            // Pinger
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(230, 207);
+            Controls.Add(ping_button);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Pinger";
+            Text = "Pinger";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button ping_button;
+    }
+}
---
+++ TimeCalculator/Pinger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeCalculator
+{
+    public partial class Pinger : Form
+    {
+        public Pinger()
+        {
+            InitializeComponent();
+        }
+        public void press_ping()
+        {
+            ping_button.PerformClick();
+        }
+
+        private async void ping_button_Click(object sender, EventArgs e)
+        {
+            Ping pingSender = new Ping();
+            string server = "10.144.0.1";
+            int numberOfPings = 10;
+
+            for (int i = 0; i < numberOfPings; i++)
+            {
+                PingReply reply = await pingSender.SendPingAsync(server);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    textBox1.AppendText("Ping to " + reply.Address.ToString() + " Successful" + " Response delay = " + reply.RoundtripTime.ToString() + " ms" + Environment.NewLine);
+                }
+                else
+                {
+                    textBox1.AppendText("Ping failed." + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
